61bb5d2eac9ce73e84ed020fb86290ac003f3070
xiekuozhi <xiekuozhi@163.com>
2022-06-17 16:44:45 +0800

    我要练习了

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,13 +1,26 @@
 <template>
    <Layout>
-            <p>我是Statistics</p>
+       <Types class="x"/>
     </Layout>     
 </template>
 
-<script lang="ts">
+<style scoped lang="scss">
+.x ::v-deep li{
+  background: white;
+  &.selected{
+    background: #c4c4c4;
+  }
+} 
+</style>
 
-export default {
-  name: "Statistics",
-};
+<script lang="ts">
+import Types from "@/components/Money/Types.vue";
+import Vue from 'vue'
+import Component from "vue-class-component";
+
+@Component({components:{Types}})
+export default class Statistics extends Vue {
+
+}
 </script>
 
